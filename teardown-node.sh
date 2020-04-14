8cc59787793f4d1d90ebe6fcc6feefe90bd81d88
Satoru Takeuchi <satoru.takeuchi@gmail.com>
2020-04-14 21:20:59 +0000

    prevent keep scratch device locked when the finalizer is ran before OSD creation
    Signed-off-by: Satoru Takeuchi <satoru.takeuchi@gmail.com>

--- teardown-node.sh
+++ teardown-node.sh
@@ -5,6 +5,7 @@ DISK=/dev/sdb
 rm -rf /var/lib/rook
 /sgdisk --zap-all $DISK
 dd if=/dev/zero of="$DISK" bs=1M count=100 oflag=direct,dsync
-ls /dev/mapper/ceph-* | xargs -I% -- /dmsetup remove %
+FILES=$(ls /dev/mapper/ceph-* 2>/dev/null || :)
+echo "${FILES}" | xargs -I% -- /dmsetup remove %
 rm -rf /dev/mapper/ceph-*
 rm -rf /dev/ceph-*
