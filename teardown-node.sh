5a8b780a4197a5db9ae24209d9b844fbd73d7091
Satoru Takeuchi <satoru.takeuchi@gmail.com>
2020-05-04 03:21:07 +0000

    fix sgdisk path

--- teardown-node.sh
+++ teardown-node.sh
@@ -3,7 +3,7 @@
 DISK=/dev/sdb
 
 rm -rf /var/lib/rook
-/sgdisk --zap-all $DISK
+sgdisk --zap-all $DISK
 dd if=/dev/zero of="$DISK" bs=1M count=100 oflag=direct,dsync
 FILES=$(ls /dev/mapper/ceph-* 2>/dev/null || :)
 echo "${FILES}" | xargs -I% -- /dmsetup remove %
