d26b5cb4b02d2adba9c7de9eef6038cfa40d18ee
Satoru Takeuchi <satoru.takeuchi@gmail.com>
2020-06-09 15:35:02 +0000

    reproduce 5504
    https://github.com/rook/rook/issues/5504
    
    Signed-off-by: Satoru Takeuchi <satoru.takeuchi@gmail.com>

--- init.sh
+++ init.sh
@@ -1,5 +1,7 @@
 #!/bin/bash -xe
 
+sudo parted /dev/sdb -s mklabel gpt
+sudo parted /dev/sdb -s mkpart CEPH_DATA ext4 0 6G
 kind create cluster
 kubectl apply -f manifests/common.yaml
 kubectl apply -f manifests/operator.yaml
