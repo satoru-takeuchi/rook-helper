d7388b19ac8142e792f3a1ad8f63995d57995e6a
Satoru Takeuchi <satoru.takeuchi@gmail.com>
2020-08-03 01:29:10 +0000

    update
    Signed-off-by: Satoru Takeuchi <satoru.takeuchi@gmail.com>

--- ci-init.sh
+++ ci-init.sh
@@ -11,4 +11,5 @@ export PATH="/tmp/rook-tests-scripts-helm/linux-amd64:$PATH" \
     TEST_BASE_DIR="$(pwd)" \
     STORAGE_PROVIDER_TESTS=ceph \
     TEST_SCRATCH_DEVICE=/dev/sdb
-_output/tests/linux_amd64/integration -test.v -test.timeout 7200s -test.run 'TestCephMultiClusterDeploySuite' 2>&1 | tee _output/tests/integrationTests.log
+_output/tests/linux_amd64/integration -test.v -test.timeout 7200s -test.run 'TestCephOSDOnPVCSuite' 2>&1 | tee _output/tests/integrationTests.log
+#_output/tests/linux_amd64/integration -test.v -test.timeout 7200s 2>&1 | tee _output/tests/integrationTests.log
