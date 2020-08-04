c189c453075735096ee9bcade25275daabf466af
Satoru Takeuchi <satoru.takeuchi@gmail.com>
2020-08-04 07:04:29 +0000

    update
    Signed-off-by: Satoru Takeuchi <satoru.takeuchi@gmail.com>

--- ci-fini.sh
+++ ci-fini.sh
@@ -12,3 +12,7 @@ tests/scripts/helm.sh clean
 tests/scripts/kubeadm.sh clean
 sudo rm -rf /var/lib/etcd
 sudo rm -rf /etc/kubernetes
+sudo rm -rf /var/lib/rook
+sudo vgremove --yes --force rook-integration-test-vg
+sudo pvremove --yes --force ${test_scratch_device}
+sudo dd if=/dev/zero of=/dev/sdb bs=1M count=100 oflag=dsync,direct
