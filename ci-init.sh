b0373d8af6e55e76d63f6e981a8dede339fda71e
Satoru Takeuchi <satoru.takeuchi@gmail.com>
2020-09-08 16:15:46 +0900

    update
    Signed-off-by: Satoru Takeuchi <satoru.takeuchi@gmail.com>

--- ci-init.sh
+++ ci-init.sh
@@ -10,6 +10,7 @@ export PATH="/tmp/rook-tests-scripts-helm/linux-amd64:$PATH" \
     TEST_HELM_PATH=/tmp/rook-tests-scripts-helm/linux-amd64/helm \
     TEST_BASE_DIR="$(pwd)" \
     STORAGE_PROVIDER_TESTS=ceph \
+    TEST_IS_OFFICIAL_BUILD=false \
     TEST_SCRATCH_DEVICE=/dev/sdb
 _output/tests/linux_amd64/integration -test.v -test.timeout 7200s -test.run 'MultiClusterDeploySuite' 2>&1 | tee _output/tests/integrationTests.log
 #_output/tests/linux_amd64/integration -test.v -test.timeout 7200s 2>&1 | tee _output/tests/integrationTests.log
