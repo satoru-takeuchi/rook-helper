ca656ecbfb8f3203ca7885cdf134c10d376e38b8
Satoru Takeuchi <satoru.takeuchi@gmail.com>
2021-01-08 18:17:18 +0000

    extend timeout in full test
    Signed-off-by: Satoru Takeuchi <satoru.takeuchi@gmail.com>

--- ci-init.sh
+++ ci-init.sh
@@ -11,7 +11,7 @@ export TEST_BASE_DIR=WORKING_DIR \
     TEST_IS_OFFICIAL_BUILD=false \
     TEST_SCRATCH_DEVICE=/dev/sdb
 
-go test -v -timeout 1800s github.com/rook/rook/tests/integration | tee test.log
+go test -v -timeout 7200s github.com/rook/rook/tests/integration | tee test.log
 #go test -v -timeout 1800s -run CephSmokeSuite github.com/rook/rook/tests/integration | tee test.log
 #go test -v -timeout 1800s -run CephSmokeSuite github.com/rook/rook/tests/integration -testify.m TestRookClusterInstallation_SmokeTest | tee test.log
 
