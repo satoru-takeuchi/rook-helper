98ade27be1ab51c83adf14c97b9cc3aa8c20f9f6
Satoru Takeuchi <satoru.takeuchi@gmail.com>
2021-01-14 13:38:46 +0000

    fix test-fini to remove cluster and local volume correctly
    Signed-off-by: Satoru Takeuchi <satoru.takeuchi@gmail.com>

--- test-fini.sh
+++ test-fini.sh
@@ -3,11 +3,11 @@
 export KUBECONFIG=~/admin.conf
 
 kubectl delete -f cluster-on-pvc.yaml
-kubectl delete -f local.yaml
 #kubectl delete -f local2.yaml
 for i in operator crds common ; do
   kubectl delete -f cluster/examples/kubernetes/ceph/${i}.yaml
 done
+kubectl delete -f local.yaml
 sudo dd if=/dev/zero of=/dev/sdb bs=1M count=100 oflag=dsync,direct
 #sudo dd if=/dev/zero of=/dev/sdc bs=1M count=100 oflag=dsync,direct
 sudo rm -rf /var/lib/rook
