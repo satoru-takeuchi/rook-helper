b1121816c9e417a7d7b14924610a38ce35e7483d
Satoru Takeuchi <satoru.takeuchi@gmail.com>
2021-01-14 12:16:31 +0000

    add scripts to manage a rook cluster
    Signed-off-by: Satoru Takeuchi <satoru.takeuchi@gmail.com>

---
+++ test-fini.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+export KUBECONFIG=~/admin.conf
+
+kubectl delete -f cluster-on-pvc.yaml
+kubectl delete -f local.yaml
+#kubectl delete -f local2.yaml
+for i in operator crds common ; do
+  kubectl delete -f cluster/examples/kubernetes/ceph/${i}.yaml
+done
+sudo dd if=/dev/zero of=/dev/sdb bs=1M count=100 oflag=dsync,direct
+#sudo dd if=/dev/zero of=/dev/sdc bs=1M count=100 oflag=dsync,direct
+sudo rm -rf /var/lib/rook
---
+++ test-init.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+#if [ $# != 1 ] ; then
+#  echo "usage: $0 <commit>" >&2
+#  exit 1
+#fi
+
+#COMMIT=$1
+
+#git checkout $COMMIT
+export KUBECONFIG=~/admin.conf
+#make -j8 IMAGES=ceph
+#docker tag build-e388186c/ceph-amd64 rook/ceph:master
+
+for i in common crds operator ; do
+  kubectl apply -f cluster/examples/kubernetes/ceph/${i}.yaml
+done
+
+kubectl apply -f local.yaml
+#kubectl apply -f local2.yaml
+kubectl apply -f cluster-on-pvc.yaml
+#kubectl apply -f cluster/examples/kubernetes/ceph/toolbox.yaml
+kubectl -n rook-ceph get pod -w
+
+#TOOLBOXPOD=$(kubectl -n rook-ceph get pod -lapp=rook-ceph-tools -o jsonpath='{.items[0].metadata.name}')
