c0b9b58d7c55c0af8010bbf86de97d8923928a47
Satoru Takeuchi <satoru.takeuchi@gmail.com>
2022-05-17 14:09:43 +0000

    update
    Signed-off-by: Satoru Takeuchi <satoru.takeuchi@gmail.com>

--- cluster-fini.sh
+++ cluster-fini.sh
@@ -6,5 +6,5 @@ kubectl delete -f $OPERATOR_DIR/toolbox.yaml
 kubectl delete -f $CLUSTER_CR
 
 sudo rm -rf /var/lib/rook
-ls /dev/mapper/ceph-* | xargs -I% -- dmsetup remove %
+ls /dev/mapper/ceph-* | xargs -I% -- sudo dmsetup remove %
 sudo rm -rf /dev/mapper/ceph-*
