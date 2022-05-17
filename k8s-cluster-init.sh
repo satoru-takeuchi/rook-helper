518ba556361ae70859ff4aa4fe5f950f77d05c75
Satoru Takeuchi <satoru.takeuchi@gmail.com>
2022-05-17 12:27:54 +0000

    update
    Signed-off-by: Satoru Takeuchi <satoru.takeuchi@gmail.com>

--- k8s-cluster-init.sh
+++ k8s-cluster-init.sh
@@ -5,7 +5,7 @@
 CONFFILE=/etc/kubernetes/admin.conf
 
 sudo kubeadm init --pod-network-cidr 10.244.0.0/16
-sudo kubectl --kubeconfig $CONFFILE taint nodes --all node-role.kubernetes.io/control-plane-
+sudo kubectl --kubeconfig $CONFFILE taint nodes --all node-role.kubernetes.io/control-plane- node-role.kubernetes.io/master-
 sudo kubectl --kubeconfig $CONFFILE apply -f https://raw.githubusercontent.com/flannel-io/flannel/v0.16.3/Documentation/kube-flannel.yml
 sudo cp $CONFFILE ~/
 sudo chown $(id -u):$(id -g) ~/admin.conf
